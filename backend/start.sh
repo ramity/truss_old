99c4b8c6799e0e43ab1b22934c6f9fc3b59670ca
Lewis Brown <lcb005@uark.edu>
2018-02-05 00:26:43 -0600

    finally finished with containerizing

--- backend/start.sh
+++ backend/start.sh
@@ -2,19 +2,18 @@
 
 # If there is some public key in keys folder
 # then it copies its contain in authorized_keys file
-if [ "$(ls -A /git-server/keys/)" ]; then
-  cd /home/git
-  cat /git-server/keys/*.pub > .ssh/authorized_keys
-  chown -R git:git .ssh
+if [ "$(ls -A /git/keys/)" ]; then
+  cat /git/keys/*.pub > ~/.ssh/authorized_keys
+  chown -R .ssh
   chmod 700 .ssh
   chmod -R 600 .ssh/*
 fi
 
 # Checking permissions and fixing SGID bit in repos folder
 # More info: https://github.com/jkarlosb/git-server-docker/issues/1
-if [ "$(ls -A /git-server/repos/)" ]; then
-  cd /git-server/repos
-  chown -R git:git .
+if [ "$(ls -A /git/repos/)" ]; then
+  cd /git/repos
+  chown -R $USER:$USER .
   chmod -R ug+rwX .
   find . -type d -exec chmod g+s '{}' +
 fi
